732aac8c426e0fd4f54cefb90c3120af64b8cb2d
Terry Weiss <terrytweiss@gmail.com>
2016-05-03 15:36:18 -0500

    ResourcePrices is now a private inner class to Stand

--- Stand.java
+++ Stand.java
@@ -17,6 +17,13 @@ public class Stand {
     public static final double STAND_PRICE = 150.00;
     public static final double START_PRICE = 100.00;
 
+    private static final double[] LEMONS_PRICES = {1.0, 0.5, 0.33};
+    private static final double[] SUGAR_PRICES = {0.5, 0.25};
+    private static final double[] ICE_PRICES = {0.1, 0.05};
+    private static final double WATER_PRICE = 0.05;
+    private static final double CUPS_PRICE = 0.02;
+    private static final double SIGN_PRICE = 0.25;
+
     private static final int START_HOUR = 10;
     private static final int END_HOUR   = 19;
 
@@ -124,5 +131,26 @@ public class Stand {
 
     public double netProfit() {
         return money - moneyAtOpen;
+    }
+
+    private class ResourcePrices {
+        public double lemons;
+        public double sugar;
+        public double ice;
+        public double water;
+        public double cups;
+        public double signs;
+        public double costPerCup;
+
+        public ResourcePrices() {
+            super();
+            lemons = LEMONS_PRICES[Game.random.nextInt(LEMONS_PRICES.length)];
+            sugar = SUGAR_PRICES[Game.random.nextInt(SUGAR_PRICES.length)];
+            ice = ICE_PRICES[Game.random.nextInt(ICE_PRICES.length)];
+            water = WATER_PRICE;
+            cups = CUPS_PRICE;
+            signs = SIGN_PRICE;
+            costPerCup = lemons + sugar + ice + water + cups;
+        }
     }
 }
