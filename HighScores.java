ddaeec63f304b2d91be76a17fb530a86975b5027
Terry Weiss <terrytweiss@gmail.com>
2016-05-09 16:22:21 -0500

    Added more documentation

--- HighScores.java
+++ HighScores.java
@@ -17,56 +17,137 @@ import java.io.Serializable;
 import java.util.ArrayList;
 
 public class HighScores implements Serializable {
-    public static int SCORES_STORED = 5;
-    private static final long serialVersionUID = 42L;
+    private static final long serialVersionUID = 42L; //Harden against different JVM implementations
 
-    private final ArrayList<Score> scoreSheet;
+    /**
+     * Number of scores stored in high score list
+     */
+    public transient static final int SCORES_STORED = 5;
 
+    private final ArrayList<Score> scoreSheet;        //List of scores
+
+
+    /**
+     * Constructs a new High Score list with the default scores stored.
+     */
+    /*
+     * BEGIN Constructor
+     *     Initialize score sheet with an initial size of scores stored
+     * END Constructor
+     */
     public HighScores() {
-        scoreSheet = new ArrayList<>(SCORES_STORED + 1);
+        scoreSheet = new ArrayList<>(SCORES_STORED);
     }
 
+    /**
+     * Gets the number of scores currently listed
+     *
+     * @return Number of scores listed
+     */
+    /*
+     * BEGIN Scores
+     *     Return score sheet's size
+     * END Scores
+     */
     public int scores() {
         return scoreSheet.size();
     }
 
+    /**
+     * Determines whether there are any scores saved
+     *
+     * @return True if there are no scores saved
+     */
+    /*
+     * BEGIN Is Empty
+     *     Return whether score sheet is empty
+     * END Is Empty
+     */
     public boolean isEmpty() {
         return scoreSheet.isEmpty();
     }
 
-    protected Score getScore(int index) {
-        return scoreSheet.get(index);
-    }
+    /**
+     * Determines the lowest score value saved. If there are no scores, a NoScoresException will be
+     * thrown. The Java Runtime will check this exception implicitly.
+     *
+     * @throws NoScoresException If there are no scores saved
+     * @return Lowest score value
+     */
+    /*
+     * BEGIN Lowest Score
+     *     IF (Score sheet is empty) THEN
+     *         Throw no scores exception
+     *     END IF
+     *     Get lowest score and return its score value
+     * END Lowest Score
+     */
+    public double lowestScore() throws NoScoresException {
+        if (scoreSheet.isEmpty()) {
+            throw new NoScoresException();
+        }
 
-    public double lowestScore() {
         return scoreSheet.get(scoreSheet.size() - 1).score;
     }
 
+    /**
+     * Creates a new {@link Score} with the given name and score value and adds it to the list in
+     * descending order. If the list is empty, the new score will automatically be added. Otherwise,
+     * the method will iterate through the list of stored scores to see if it's higher than any of
+     * the currently-stored scores and insert where applicable. If the score still hasn't been
+     * added, it will be appended if the list isn't full.
+     *
+     * @param name   Player's name
+     * @param score  Player's score
+     * @return True if score was added
+     */
+    /*
+     * BEGIN Add Score
+     *     Initialize added to false
+     *     Store size of score sheet as scores
+     *     IF (there aren't any scores) THEN
+     *         Create and add new Score
+     *         Set added to true
+     *     ELSE
+     *         FOR (each score or until one is added)
+     *             IF (score value is higher than current score's score value) THEN
+     *                 IF (score sheet is full) THEN
+     *                     Remove lowest score
+     *                 END IF
+     *                 Create and insert new Score at current index
+     *                 Set added to true
+     *             END IF
+     *         END FOR
+     *         IF (Score wasn't added and the list isn't full) THEN
+     *             Create and append new score sheet
+     *             Set added to true
+     *         END IF
+     *     END IF
+     *     Return whether score was added
+     * END Add Score
+     */
     public boolean addScore(String name, double score) {
-        boolean added = false;
-        int scores;
-
-        Score newScore = new Score(name, score);
+        boolean added = false;  //Whether score was added
+        int scores;             //Number of scores currently stored in list
 
         scores = scoreSheet.size();
         if (scores == 0) {
-            scoreSheet.add(newScore);
+            scoreSheet.add(new Score(name, score));
             added = true;
         } else {
             for (int i = 0; i < scores && !added; i++) {
-                if (newScore.score > scoreSheet.get(i).score) {
-                    scoreSheet.add(i, newScore);
-
-                    if (scoreSheet.size() == SCORES_STORED + 1) {
-                        scoreSheet.remove(SCORES_STORED);
+                if (score > scoreSheet.get(i).score) {
+                    if (scoreSheet.size() == SCORES_STORED) {
+                        scoreSheet.remove(SCORES_STORED - 1);
                     }
 
+                    scoreSheet.add(i, new Score(name, score));
                     added = true;
                 }
             }
 
             if (!added && scoreSheet.size() < SCORES_STORED) {
-                scoreSheet.add(newScore);
+                scoreSheet.add(new Score(name, score));
                 added = true;
             }
         }
@@ -74,9 +155,28 @@ public class HighScores implements Serializable {
         return added;
     }
 
+    /**
+     * Creates and returns a String display of the high score sheet. If there are no scores entered,
+     * a message will say so instead.
+     *
+     * @return String representation of the high score list
+     */
+    /**
+     * BEGIN To String
+     *     IF (Score sheet is empty) THEN
+     *         Append message that there aren't any high scores
+     *     ELSE
+     *         Append high scores title
+     *         FOR (each score)
+     *             Append formatted string with score's name and score value
+     *         END FOR
+     *     END IF
+     *     Return built string
+     * END To String
+     */
     @Override
     public String toString() {
-        StringBuilder out = new StringBuilder();
+        StringBuilder out = new StringBuilder();    //Stringbuild to build output string
 
         if (scoreSheet.isEmpty()) {
             out.append("There currently aren't any high scores.\n");
@@ -84,28 +184,45 @@ public class HighScores implements Serializable {
             out.append("            High Scores\n");
 
             for (Score score : scoreSheet) {
-                out.append(String.format("Name: %-15s    Score: $%.2f", score.name, score.score));
+                out.append(String.format("Name: %-15s    Score: $%.2f%n", score.name, score.score));
             }
         }
 
         return out.toString();
     }
 
+    /**
+     * Creates and loads a high score list from the given filename. The file must contain a
+     * serialized copy of a previous High Score object. If the file can't be read or
+     * doesn't exist, an empty high score list will be created.
+     *
+     * @param filename Name of file to read from
+     * @return New high score loaded, or empty one if unable to read
+     */
+    /*
+     * BEGIN Load High Scores
+     *     TRY
+     *         Init object input stream from file input stream from filename
+     *         Read and cast file object as a High Score and store it
+     *     CATCH (IO Exception)
+     *         Create and store new high scores object
+     *     FINALLY
+     *         Close all streams
+     *     END TRY-CATCH
+     *     Return new score sheet
+     * END Load High Scores
+     */
     public static HighScores loadHighScores(String filename) {
-        HighScores scores;
-        FileInputStream fis = null;
-        ObjectInputStream ois = null;
+        HighScores scores;              //New high scores object
+        ObjectInputStream ois = null;   //Object stream reader
 
         try {
-            fis = new FileInputStream(filename);
-            ois = new ObjectInputStream(fis);
+            ois = new ObjectInputStream(new FileInputStream(filename));
             scores = (HighScores)ois.readObject();
         } catch (IOException | ClassNotFoundException e) {
             scores = new HighScores();
         } finally {
             try {
-                if (fis != null)
-                    fis.close();
                 if (ois != null)
                     ois.close();
             } catch (IOException e) {
@@ -116,48 +233,108 @@ public class HighScores implements Serializable {
         return scores;
     }
 
+    /**
+     * Saves the given high scores object to the given filename.
+     *
+     * @param filename    File to save high scores to
+     * @param highScores  High scores to save
+     * @return Whether high scores were able to be saved
+     */
+    /*
+     * START Save High Scores
+     *     Set saved to false
+     *     IF (high scores isn't null) THEN
+     *         TRY
+     *             Create object stream writer from file stream writer from filename
+     *             Write high score object to file
+     *             Set saved to true
+     *         CATCH (IO Exception)
+     *             Do nothing
+     *         FINALLY
+     *             Close all streams
+     *         END TRY-CATCH
+     *     END IF
+     *     Return whether scores were saved successfully
+     * END Save High Scores
+     */
     public static boolean saveHighScores(String filename, HighScores highScores)
     {
-        boolean loaded = false;
+        boolean saved = false;          //Whether score was saved
+        ObjectOutputStream oos = null;  //Object stream writer
 
         if (highScores != null) {
-            FileOutputStream fos = null;
-            ObjectOutputStream oos = null;
-
             try {
-                fos = new FileOutputStream(filename);
-                oos = new ObjectOutputStream(fos);
+                oos = new ObjectOutputStream(new FileOutputStream(filename));
                 oos.writeObject(highScores);
-                loaded = true;
+                saved = true;
             } catch (IOException e) {
-                //Loaded will stay false
+                //Saved will stay false
             } finally {
                 try {
                     if (oos != null)
                         oos.close();
-                    if (fos != null)
-                        fos.close();
                 } catch (IOException e) {
                     //Do nothing
                 }
             }
         }
 
-        return loaded;
+        return saved;
     }
 
+
+    /**
+     * This exception should be thrown whenever there is a check for a score on an empty high score
+     * list.
+     */
+    private class NoScoresException extends IndexOutOfBoundsException {
+        /**
+         * Constructs the No Scores exception
+         */
+        /*
+         * BEGIN Constructor
+         *     Create IndexOutOfBoundsException using no scores saved message
+         * END Constructor
+         */
+        private NoScoresException() {
+            super("No scores are saved");
+        }
+    }
+
+    /**
+     * This is a demo to create and display a high score using random scores to prove it works
+     * easily and visibly.
+     *
+     * @param args Console arguments aren't supported
+     */
+    /*
+     * BEGIN Main
+     *     Warm up RNG with 100 iterations
+     *     FOR (10 scores)
+     *         Generate a random score
+     *         Display score
+     *         Add new score
+     *         Display all scores so far
+     *         Display lowest score to make sure that works
+     *     END FOR
+     * END Main
+     */
     public static void main(String[] args) {
-        double random;
-        HighScores scores = new HighScores();
+        double random;                              //Random score
+        HighScores scores = new HighScores();       //High scores object to be used
+
+        for (int i = 0; i < 100; i++) {
+            Game.random.nextInt();
+        }
 
         for (int i = 0; i < 10; i++) {
             random = Math.random() * 100;
             System.out.println("Trying " + random);
 
-            scores.addScore("abc", random);
+            scores.addScore("Somebody", random);
 
-            System.out.println(scores);
-            System.out.println(scores.lowestScore());
+            System.out.print(scores);
+            System.out.println("Lowest score: " + scores.lowestScore() + "\n");
         }
     }
 }
--- Launcher.java
+++ Launcher.java
@@ -10,17 +10,52 @@ package lemonadestand;
 
 import java.util.Scanner;
 
+/**
+ * The launcher class launches the program and allows the player to continue to play new games until
+ * they want to quit. The player is also able to see the high scores list from here. This class
+ * does not require state and should not be instantiated.
+ *
+ * @author Terry Weiss
+ */
 public class Launcher {
-    private final static Scanner scanner = new Scanner(System.in);
-    private final static String SCORES_FILE = "highscores.sco";
-    private final static int INVALID_OPTION = 0;
-    private final static int NEW_GAME    = 1;
-    private final static int HIGH_SCORES = 2;
-    private final static int QUIT        = 3;
+    private final static Scanner scanner = new Scanner(System.in);  //Scanner object to read input
+    private final static String SCORES_FILE = "highscores.sco";     //High scores file
 
+    //Menu option constants
+    private final static int INVALID_OPTION = 0;    //Invalid menu option number
+    private final static int FIRST_OPTION   = 1;    //First menu option
+    private final static int LAST_OPTION    = 3;    //Last menu option
+    private final static int NEW_GAME    = 1;       //New game option
+    private final static int HIGH_SCORES = 2;       //High scores option
+    private final static int QUIT        = 3;       //Quit option
+
+
+    /**
+     * The main method launches the lemonade stand program.
+     *
+     * @param args Console arguments aren't supported
+     */
+    /*
+     * BEGIN Main
+     *     Load and store high scores from scores file
+     *     "Warm up" random number generator
+     *     DO WHILE (option isn't quit)
+     *         Prompt menu option
+     *         SWITCH (menu option)
+     *             CASE New Game:      Start game with high scores object
+     *             CASE High Scores:   Display high scores
+     *             CASE Quit:          Save high scores
+     *         END SWITCH
+     *     END DO-WHILE
+     * END Main
+     */
     public static void main(String[] args) {
-        HighScores highScores = HighScores.loadHighScores(SCORES_FILE);
-        int option;
+        HighScores highScores;  //High scores list
+        int option;             //Menu option selection
+
+        highScores = HighScores.loadHighScores(SCORES_FILE);
+
+        warmupRandomGenerator();
 
         do {
             option = promptMenuOption();
@@ -39,8 +74,35 @@ public class Launcher {
         } while (option != QUIT);
     }
 
+    /**
+     * Prompts for a menu option. If the player enters an invalid option, they will be re-prompted.
+     *
+     * @return Menu option selected
+     */
+    /*
+     * START Menu Option
+     *
+     * END Prompt Menu Option
+     */
+    /*
+     * BEGIN Prompt Menu Option
+     *     Display menu
+     *     DO WHILE (menu option is out of bounds)
+     *         TRY
+     *             Prompt for option
+     *         CATCH (Invalid number)
+     *             Do nothing
+     *         END TRY-CATCH
+     *
+     *         IF (option is out of bounds)
+     *             Display option out of bounds
+     *         END IF
+     *     END DO-WHILE
+     *     Return option
+     * END Prompt Menu Option
+     */
     private static int promptMenuOption() {
-        int option = INVALID_OPTION;
+        int option = INVALID_OPTION;    //Selected menu option
 
         System.out.println("          Lemonade Stand");
         System.out.println("    1. Start a new game");
@@ -48,18 +110,17 @@ public class Launcher {
         System.out.println("    3. Quit");
 
         do {
-            System.out.print("Enter an option: ");
-
             try {
+                System.out.print("Enter an option: ");
                 option = Integer.parseInt(scanner.nextLine());
             } catch (NumberFormatException e) {
                 //Option will stay invalid
             }
 
-            if (option < NEW_GAME || option > QUIT) {
+            if (option < FIRST_OPTION || option > LAST_OPTION) {
                 System.out.print("That isn't a valid option. ");
             }
-        } while (option < NEW_GAME || option > QUIT);
+        } while (option < FIRST_OPTION || option > LAST_OPTION);
 
         return option;
     }
@@ -79,5 +140,14 @@ public class Launcher {
         }
 
         HighScores.saveHighScores(SCORES_FILE, highScores);
+    }
+
+    private static void warmupRandomGenerator() {
+        for (int i = 0; i < 1000; i++) {
+            Game.random.nextInt();
+        }
+    }
+
+    private Launcher() {
     }
 }
